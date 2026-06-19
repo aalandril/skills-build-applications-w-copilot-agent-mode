f009d97d7af558d9adf3aeaec75c24d08d21b858
aalandril <aalandril@gmail.com>
2026-06-19 17:45:39 +0000

    Added ending slash

--- octofit-tracker/frontend/src/components/Activities.tsx
+++ octofit-tracker/frontend/src/components/Activities.tsx
@@ -8,7 +8,7 @@ function Activities() {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch(getApiEndpoint('/api/activities'))
+    fetch(getApiEndpoint('/api/activities/'))
       .then((response) => response.json())
       .then((data) => {
         setActivities(normalizeCollectionResponse<Activity>(data));
